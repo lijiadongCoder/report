27cbed108c7d3f4110b022b8420212e321a84f95
jiadong.li <jiadong.li@jgdt.com>
2022-04-06 23:25:15 +0800

    查询健康指标7

--- src/main/java/com/tencent/wxcloudrun/controller/HealthKpiController.java
+++ src/main/java/com/tencent/wxcloudrun/controller/HealthKpiController.java
@@ -1,5 +1,7 @@
 package com.tencent.wxcloudrun.controller;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
 import com.tencent.wxcloudrun.config.ApiResponse;
 import com.tencent.wxcloudrun.model.HealthKpiDto;
 import com.tencent.wxcloudrun.service.CounterService;
@@ -7,7 +9,6 @@ import com.tencent.wxcloudrun.service.HealthKpiService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.configurationprocessor.json.JSONArray;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -41,6 +42,6 @@ public class HealthKpiController {
         logger.error("into");
         List<HealthKpiDto> list = healthKpiService.getAll();
         logger.info(list.toString());
-        return ApiResponse.ok(list);
+        return ApiResponse.ok(JSONArray.parseArray(JSON.toJSONString(list)));
     }
 }
